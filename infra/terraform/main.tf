1d91b3732cc4fb63cd5c11361aedf5f9bfeddf40
mrachuta <34251394+mrachuta@users.noreply.github.com>
2025-03-31 19:27:04 +0200

    Update backend URL and cluster scheduler

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -25,9 +25,9 @@ module "aks_cheap_cluster" {
   }
   aks_scaling_details_default_node = {
     days          = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-    start_time_HH = 17
+    start_time_HH = 16
     start_time_MM = 00
-    stop_time_HH  = 22
+    stop_time_HH  = 21
     stop_time_MM  = 30
     timezone      = "UTC"
   }
